94d4e2fb8ae7a11ae48979425be430103c5bcec9
Gabriel Abal <129793663+gabriel-abal@users.noreply.github.com>
2024-08-19 19:14:17 -0300

    Add files via upload

---
+++ src/controllers/TaskController.ts
@@ -0,0 +1,62 @@
+import { Handler, Request, Response } from "express";
+import { Task } from "../models/Task";
+import { z } from "zod";
+import { HttpError } from "../errors/HttpError";
+
+//req.body {title, description, status, priority}
+const StoreRequestSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  status: z.enum(["todo", "doing", "done"]),
+  priority: z.enum(["low", "medium", "high"]),
+});
+
+const UpdateRequestSchema = z.object({
+  title: z.string().optional(),
+  description: z.string().optional(),
+  status: z.enum(["todo", "doing", "done"]).optional(),
+  priority: z.enum(["low", "medium", "high"]).optional(),
+});
+
+export class TaskController {
+  // GET /api/tasks
+  // tipando com Handler
+  index: Handler = (req, res) => {
+    const task = Task.findAll();
+    return res.json(task);
+  };
+
+  // POST /api/tasks
+  //ou tipando com Request, Response
+  store = (req: Request, res: Response) => {
+    const parsedBody = StoreRequestSchema.parse(req.body);
+    const newTask = Task.create(parsedBody);
+    res.status(201).json(newTask);
+  };
+
+  // GET /api/task/:id
+  //(+id) o + já converte para number
+  show: Handler = (req, res) => {
+    const { id } = req.params;
+    const task = Task.findById(+id);
+    if (!task) throw new HttpError(404, "task not found");
+    res.json(task);
+  };
+
+  // PUT /api/tasks/:id
+  update: Handler = (req, res) => {
+    const { id } = req.params;
+    const parsedBody = UpdateRequestSchema.parse(req.body);
+    const updatedTask = Task.update(+id, parsedBody);
+    if (!updatedTask) throw new HttpError(404, "task not found");
+    res.json(updatedTask);
+  };
+
+  // DELETE /api/tasks/:id
+  delete: Handler = (req, res) => {
+    const { id } = req.params;
+    const deletedTask = Task.delete(+id);
+    if (!deletedTask) throw new HttpError(404, "task not found");
+    res.json(deletedTask);
+  };
+}
